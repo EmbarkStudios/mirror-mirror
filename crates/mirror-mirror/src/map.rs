ee33135f3d925640d7d072956a4122e74125aebb
David Pedersen <david.pdrsn@gmail.com>
2022-12-16 11:34:29 +0100

    expand on why `HashMap` isn't supported

--- crates/mirror-mirror/src/map.rs
+++ crates/mirror-mirror/src/map.rs
@@ -11,6 +11,10 @@ use crate::Reflect;
 ///
 /// [`BTreeMap`]: alloc::collections::BTreeMap
 /// [`HashMap`]: std::collections::HashMap
+// HashMap isn't supported because we need a `Value` variant for map values. The most obvious
+// choice is `enum Value { Map(HashMap<Value, Value>) }`. However now `Value` is used as the key in
+// a `HashMap` so it most implement `Hash + Eq` but it can't since it contains a `HashMap` which
+// doesn't implement `Hash + Eq`, because there is no stable iteration order.
 pub trait Map: Reflect {
     fn get(&self, key: &dyn Reflect) -> Option<&dyn Reflect>;
 
