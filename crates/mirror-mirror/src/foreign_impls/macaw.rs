0a56a46f048184bb615bcd22f829cd53f6da3335
Max de Danschutter <m.danschutter@gmail.com>
2024-02-22 14:45:15 +0100

    implement macaw type

--- crates/mirror-mirror/src/foreign_impls/macaw.rs
+++ crates/mirror-mirror/src/foreign_impls/macaw.rs
@@ -2,6 +2,6 @@ use macaw::ColorRgba8;
 use mirror_mirror_macros::__private_derive_reflect_foreign;
 
 __private_derive_reflect_foreign! {
-    #[reflect(crate_name(crate))]
+    #[reflect(crate_name(crate), opt_out(Default))]
     pub struct ColorRgba8(pub [u8; 4]);
 }
